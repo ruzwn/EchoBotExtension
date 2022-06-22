2d1644da3e6eb75421dc0a769c3dd67847719c05
ruzwn <ruzwn@users.noreply.github.com>
2022-06-22 15:36:57 +0300

    Initial commit.

---
+++ EchoBotExtension/ParseUtils.cs
@@ -0,0 +1,54 @@
+﻿namespace EchoBotExtension
+{
+	public static class ParseUtils
+	{
+		private static readonly char[] Operations = {'+', '-', '*', '/', '%'};
+
+		public static bool TryParseArithmeticOperation(
+			string source, out int num1, out int num2, out int result, out char operation)
+		{
+			num1 = num2 = result = 0;
+			operation = ' ';
+			
+			// Find operator in string and send substring before and after operator to int.TryParse()
+			var operationIndex = source.IndexOfAny(Operations);
+			if (operationIndex == -1)
+			{
+				return false;
+			}
+
+			var s1 = source.Substring(0, operationIndex);
+			var s2 = source.Substring(operationIndex + 1);
+			operation = source[operationIndex];
+
+			if (int.TryParse(s1, out num1) && int.TryParse(s2, out num2))
+			{
+				try
+				{
+					result = GetResult(num1, num2, operation);
+					
+					return true;
+				}
+				catch
+				{
+					return false;
+				}
+			}
+
+			return false;
+		}
+		
+		private static int GetResult(int num1, int num2, char operation)
+		{
+			return operation switch
+			{
+				'+' => num1 + num2,
+				'-' => num1 - num2,
+				'*' => num1 * num2,
+				'/' => num1 / num2,
+				'%' => num1 % num2,
+				_ => 0
+			};
+		}
+	}
+}
---
+++ EchoBotExtension/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EchoBotExtension
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Console EchoBot is online. I will repeat any message you send me!");
+            Console.WriteLine("Say \"quit\" to end.");
+
+            // Create the Console Adapter, and add Conversation State
+            // to the Bot. The Conversation State will be stored in memory.
+            var adapter = new ConsoleAdapter();
+
+            // Create the instance of our Bot.
+            var echoBot = new EchoBot();
+
+            // Connect the Console Adapter to the Bot.
+            adapter.ProcessActivityAsync(
+                async (turnContext, cancellationToken) => await echoBot.OnTurnAsync(turnContext, cancellationToken)).Wait();
+        }
+    }
+}
